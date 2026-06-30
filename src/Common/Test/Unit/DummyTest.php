db4f100d8db60f8bad70d3f6d9503ea3c055daad
Dominik Adamski <dominik04u@o2.pl>
2026-06-30 14:57:26 +0200

    testing setup

---
+++ src/Common/Test/Unit/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\Test\Unit;
+
+class DummyTest extends \PHPUnit\Framework\TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
